468c18e69926fb88cbad680441b01be2ec4a2a5e
kivitasku <158566850+kivitasku@users.noreply.github.com>
2026-09-07 10:23:29 +0300

    fixed import scripts

--- run-import.sh
+++ run-import.sh
@@ -14,3 +14,14 @@ fi
 
 echo
 echo "Music import completed successfully."
+
+npx tsx scripts/updateAlbumCovers.ts
+
+if [ $? -ne 0 ]; then
+    echo
+    echo "Album cover update failed."
+    exit 1
+fi
+
+echo
+echo "Album cover update completed successfully."
